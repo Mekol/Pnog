b55a5d70eda86140fbb28380546bebcc7f05fd51
mkl <mikul.bm@gmail.com>
2019-03-20 23:11:30 +0100

    draw view example

--- app/src/main/java/com/mkl/pnog/DrawView.kt
+++ app/src/main/java/com/mkl/pnog/DrawView.kt
@@ -7,7 +7,7 @@ import android.util.AttributeSet
 import android.view.View
 import java.security.AccessControlContext
 
-class DrawView(context: Context,attrs: AttributeSet) : View(context,attrs) {
+class DrawView(context: Context) : View(context) {
 
 val paint = Paint()
 
@@ -19,7 +19,14 @@ val paint = Paint()
     }
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-
+       // super.onDraw(canvas)
+        canvas?.drawRGB (255, 255, 255)
+        val width = getWidth ()
+        val hieght = getHeight ()
+        val brush1 = Paint ()
+        brush1.setARGB (255, 255, 0, 0)
+        brush1.setStyle (Paint.Style.STROKE)
+        for (f in 0..9)
+            canvas?.drawCircle ((width / 2) .toFloat (), (hieght / 2) .toFloat (), (f * 15) .toFloat (), brush1)
     }
 }
--- app/src/main/java/com/mkl/pnog/game1p.kt
+++ app/src/main/java/com/mkl/pnog/game1p.kt
@@ -2,11 +2,15 @@ package com.mkl.pnog
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 
 class game1p : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game1p)
+        val layout = findViewById<ConstraintLayout>(R.id.constLay)
+        val gameView = DrawView(this)
+        layout.addView(gameView)
     }
 }
